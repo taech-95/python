3542e26a6c3ee8cffcb4d579fb9a8c84d8e898db
Mykola <mykola.maksymov@gmail.com>
2021-03-29 19:18:58 +0200

    Day 31 complete

--- Intermediate/Day 31 Flash Card App/main.py
+++ Intermediate/Day 31 Flash Card App/main.py
@@ -3,14 +3,19 @@ from tkinter import *
 import pandas
 import random
 
-data = pandas.read_csv("data/french_words.csv")
-data_dictionary = data.to_dict(orient="records")
-
-# print(data)
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    data_dictionary = original_data.to_dict(orient="records")
+else:
+    data_dictionary = data.to_dict(orient="records")
+my_timer = None
+current_card = {}
 
 
 def generate_card():
-    #Version 1
+    # Version 1
     # random_word = random.randint(0, len(data_dictionary) - 1)
     # list_of_words_french = []
     # list_of_words_english = []
@@ -24,13 +29,29 @@ def generate_card():
     # canvas.itemconfig(word_text, text=random_french_word)
     # canvas.itemconfig(title_text, text="French")
     # Version 2
-    random_french_word = random.choice(data_dictionary)
-    canvas.itemconfig(word_text, text=random_french_word["French"])
-    canvas.itemconfig(title_text, text="French")
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(data_dictionary)
+    canvas.itemconfig(word_text, text=current_card["French"], fill="black")
+    canvas.itemconfig(title_text, text="French", fill="black")
+    canvas.itemconfig(canvas_background, image=background)
+    flip_timer = window.after(3000, flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(title_text, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_background, image=english_background)
+
+def is_known():
+    data_dictionary.remove(current_card)
+    data = pandas.DataFrame(data_dictionary)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    generate_card()
 
 window = Tk()
 window.title("Flash Card App")
-    # window.minsize(300, 300)
+# window.minsize(300, 300)
 window.minsize(width=900, height=750)
 window["bg"] = BACKGROUND_COLOR
 window["padx"] = 50
@@ -38,7 +59,8 @@ window["pady"] = 50
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 background = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 275, image=background)
+english_background = PhotoImage(file="images/card_back.png")
+canvas_background = canvas.create_image(400, 275, image=background)
 canvas.grid(row=1, column=0, pady=20, columnspan=2)
 title_text = canvas.create_text(400, 125, text="Title", fill="black", font=("Arial", 40, "italic"))
 word_text = canvas.create_text(400, 275, text="Word", fill="black", font=("Arial", 60, "bold"))
@@ -49,8 +71,9 @@ right_image = PhotoImage(file="images/right.png")
 btn_wrong = Button(image=wrong_image, highlightthickness=0, command=generate_card)
 btn_wrong.grid(column=0, row=2)
 
-btn_right = Button(image=right_image, highlightthickness=0, command=generate_card)
+btn_right = Button(image=right_image, highlightthickness=0, command=is_known)
 btn_right.grid(column=1, row=2)
+flip_timer = window.after(3000, flip_card)
 generate_card()
 
 window.mainloop()
