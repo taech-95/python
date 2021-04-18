f8b023d5d1a1364775ddb7ccce5e25f50ac6ebce
Mykola <mykola.maksymov@gmail.com>
2021-04-18 22:15:44 +0200

    Update main.py

--- Intermediate/Day35_API_Keys/main.py
+++ Intermediate/Day35_API_Keys/main.py
@@ -3,7 +3,7 @@ import requests
 my_email = "ivasik.morozov1999@gmail.com"
 password = "password"
 
-api_key = "e80fb3bc72b13784a12e6b4409b5fe3a"
+api_key = "..."
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 
 params = {
