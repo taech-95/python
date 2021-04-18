232a28efaf5ee3b70313e26bc48b5689fda885c2
Mykola <mykola.maksymov@gmail.com>
2021-04-18 18:58:51 +0200

    Day 35

--- Intermediate/Day35_API_Keys/main.py
+++ Intermediate/Day35_API_Keys/main.py
@@ -1,3 +1,40 @@
 import requests
+
+my_email = "ivasik.morozov1999@gmail.com"
+password = "QAZ123wertfasny"
+
 api_key = "e80fb3bc72b13784a12e6b4409b5fe3a"
-response = requests.get()
+endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+
+params = {
+    "lat": 52.237049,
+    "lon": 21.017532,
+    "appid": api_key,
+    "exclude": "current,minutely,daily,alerts"
+}
+
+response = requests.get(url=endpoint, params=params)
+response.raise_for_status()
+data = response.json()
+slicer = data["hourly"][:13]
+print(data["hourly"][0]["weather"][0]["id"])
+print(slicer)
+weather_id =[]
+will_rain = False
+for hour_data in slicer:
+    condition = int(hour_data["weather"][0]["id"])
+    if condition < 700:
+        weather_id.append(hour_data["weather"][0]["id"])
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella")
+
+print(weather_id)
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     print(letter_with_name)
+#     connection.sendmail(from_addr=my_email, to_addrs=birthday_person["email"],
+#                         msg=f"Subject:Happy birthday\n\n {letter_with_name}")
